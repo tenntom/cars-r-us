754d443ac9cbda884bfdb335933c027ebcdd3131
Tom Clouse <tommyclouse@yahoo.com>
2021-05-04 15:38:15 -0500

    fixed date

--- scripts/orders.js
+++ scripts/orders.js
@@ -37,9 +37,19 @@ const shoppingCart = (order) => {
         currency: "USD"
     })
 
-    const dateObject = order.timestamp
+    const dateObject = new Date(order.timestamp)
+    //     hour: '2 digit',
+    //     minute: '2-digit',
+    //     second: '2-digit'
+    // }
+    // )
+    
+    // order.timestamp
 
     const timeFormatted = dateObject.toLocaleString()
+
+    
+
     // const timeFormatted = dateObject.toLocaleString("en-US", {
     //     day: "numeric",
     //     year: "numeric",
@@ -48,16 +58,14 @@ const shoppingCart = (order) => {
     //     second: "numeric"
     // })
     // const timeFormatted = dateObject.toLocaleString("en-US", {
-    //     hour: '2 digit',
-    //     minute: '2-digit',
-    //     second: '2-digit'
+
     // })
 
 
 
     return `
     <li>
-    Order ${order.id} was placed on ${timeFormatted} and includes ${foundWheel.wheelColor} ${foundWheel.style} wheels,  ${foundPaint.color} paint, a ${foundTech.tech} dashboard, and ${foundSeat.color} ${foundSeat.material} seats. The total cost was ${costString}.
+    Order ${order.id} was placed on ${dateObject} and includes ${foundWheel.wheelColor} ${foundWheel.style} wheels,  ${foundPaint.color} paint, a ${foundTech.tech} dashboard, and ${foundSeat.color} ${foundSeat.material} seats. The total cost was ${costString}.
     </li>
     `
 }
